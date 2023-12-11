3af32a0ebf85e6fba64d16e27b6d5c161d35a07f
Nikolay <komissariat@mail.ru>
2023-12-11 22:47:46 +0300

    The third task has been completed

---
+++ lesson_3/src/solution_1.py
@@ -0,0 +1,3 @@
+products = [1, 14, 4, 8, 6, 9]
+res: int = sum(products)
+print(res)
---
+++ lesson_3/src/solution_2.py
@@ -0,0 +1,7 @@
+name_1: str = "Света"
+name_2: str = "Олег"
+name_3: str = "Маша"
+name_4: str = "Паша"
+print(" ")
+print("В четные дни работают: ", name_1, name_2)
+print("В нечетные дни работают: ", name_3, name_4)
