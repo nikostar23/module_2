5a55e1fea9d4f5b79724271522f5b5737c39c329
Nikolay <komissariat@mail.ru>
2023-12-12 23:20:18 +0300

    changed the 2nd task in the 3rd lesson

--- lesson_3/src/solution_2.py
+++ lesson_3/src/solution_2.py
@@ -2,6 +2,7 @@ name_1: str = "Света"
 name_2: str = "Олег"
 name_3: str = "Маша"
 name_4: str = "Паша"
+worker = [name_1, name_2, name_3, name_4]
 print(" ")
-print("В четные дни работают: ", name_1, name_2)
-print("В нечетные дни работают: ", name_3, name_4)
+print("В четные дни работают: ", worker[::2])
+print("В нечетные дни работают: ", worker[1::2])
