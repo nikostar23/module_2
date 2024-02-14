edd96a38346166dfa98799ebf35b852294d4bbd3
Nikolay <komissariat@mail.ru>
2024-02-14 22:33:44 +0300

    15 task is correct

--- lesson_15/src/solution_1.py
+++ lesson_15/src/solution_1.py
@@ -1,21 +1,18 @@
-def add_Client(name: str, history: dict):
-  
-  client_id = len(history) + 1
+def add_client(name: str, history: dict):
+    client_id = len(history) + 1
+    history[client_id] = {'name': name, 'orders': []}
 
-  history[client_id] = {'name': name, 'orders': []}
 
-def Make_order(client_id: int, history: dict, order_details: dict):
-  
-  history[client_id]['orders'].append(order_details)
+def make_order(client_id: int, history: dict, order_details: dict):
+    history[client_id]['orders'].append(order_details)
 
-def GET_HISTORY(client_id: int, history: dict):
 
-  return history[client_id]['orders']
+def get_history(client_id: int, history: dict):
+    return history[client_id]['orders']
+
 
 client_history = {}
 
-add_Client('Roman', client_history)
-
-Make_order(1, client_history, {'order_id': 1, 'amount': 100})
-
-print(GET_HISTORY(1, client_history))
+add_client('Roman', client_history)
+make_order(1, client_history, {'order_id': 1, 'amount': 100})
+print(get_history(1, client_history))
--- lesson_15/src/solution_2.py
+++ lesson_15/src/solution_2.py
@@ -1,28 +1,30 @@
-def Update_stock(item: str, quantity: int, stock: dict):
-   if item in stock:
-    stock[item]['quantity'] += quantity
-
-   else:
-    stock[item] = {'quantity': quantity}
+def update_stock(item: str, quantity: int, stock: dict):
+    if item in stock:
+        stock[item]['quantity'] += quantity
     
-def get_Item_quantity(item_name, stock: dict):
-   if item_name in stock:
-     return stock[item_name]['quantity']
-   else:
-      return 0
+    else:
+        stock[item] = {'quantity': quantity}
+    
 
-def REMOVE_item(item, stock):
+def get_item_quantity(item_name, stock: dict):
+    if item_name in stock:
+        return stock[item_name]['quantity']
+    else:
+        return 0
+
+
+def remove_item(item, stock):
     if item in stock:
         del stock[item]
 
 stock = {}
 
-Update_stock('Apple', 50, stock)
-Update_stock('Banana', 30, stock)
-Update_stock('Coffee', 20, stock)
+update_stock('Apple', 50, stock)
+update_stock('Banana', 30, stock)
+update_stock('Coffee', 20, stock)
 
-print(get_Item_quantity('Apple', stock))
+print(get_item_quantity('Apple', stock))
 
-REMOVE_item('Banana', stock)
+remove_item('Banana', stock)
 
 print(stock)
